6b74ebe60ef2bb0e6118f0cdee4906614a58c4b6
leftygray <leftygray@gmail.com>
2020-05-10 09:51:07 +1000

    Updated results and figure generation script

--- wpro-results.r
+++ wpro-results.r
@@ -3,19 +3,16 @@
 source("utils/extract-results.r")
 
 ##JOBIDs
-dateDir <- "2020-04-24"
-JOBID <- "928744"
-# PHL-MYS <- "1117069" # Quick
-# no_NPIs <- "36285"" # Quick
-
-## doublePHL <- "64383" #Full
-## PHL-MYS-noNCD <- "785075" # Quick
-
+dateDir <- "2020-05-07"
+JOBID <- "171055"
+StanModel <- "base_wpro"
 # Load files and results
 resultsDir <- paste0("results/DateRep-", dateDir, "/")
-filename <- paste0('base-',JOBID,"-stanfit.Rdata")
+figuresDir <- paste0("figures/DateRep-", dateDir, "/")
+filename <- paste0(StanModel, '-',JOBID,"-stanfit.Rdata")
 # filename <- paste0('base-',JOBID,".Rdata")
-print(sprintf("loading: %s",paste0("results/DateRep-", dateDir, "/", filename)))
+print(sprintf("loading: %s",paste0("results/DateRep-", dateDir, "/", 
+  filename)))
 load(paste0("results/DateRep-", dateDir, "/", filename))
 
 # system(paste0("Rscript covariate-size-effects.r ", "DateRep-", dateDir, "/", filename,
@@ -51,10 +48,11 @@ for(ii in 1:nCountries) {
     write.csv(data_country, paste0(figuresDir, 'SummaryResults-',JOBID,
       '-',countries[[ii]],'.csv'))
     
-    country_covariates <- CountryCovariates(country, covariates,data_country$rt_max)
+    country_covariates <- CountryCovariates(country, covariates,
+      data_country$rt_max)
     
-    write.csv(country_covariates, paste0(figuresDir, 'Interventions-',JOBID,
-      '-',countries[[ii]],'.csv'))
+    write.csv(country_covariates, paste0(figuresDir, 'Interventions-',
+      JOBID, '-',countries[[ii]],'.csv'))
     
     make_plots(data_country = data_country, 
       covariates_country_long = country_covariates,
@@ -74,12 +72,12 @@ for(ii in 1:nCountries) {
     data_country <- CountryOutputs(ii,country,dates[[ii]],reported_cases,
       deaths_by_country,prediction,estimated.deaths,rt)
     
-    data_country_forecast <- CountryForecast(ii,country,dates[[ii]],forecast,prediction,
-      estimated.deaths)
+    data_country_forecast <- CountryForecast(ii,country,dates[[ii]],
+      forecast,prediction, estimated.deaths)
     
-    make_single_plot(data_country, data_country_forecast, filename, figuresDir,
-      country, logy = FALSE, ymax = round(max(data_country$deaths), digits = -1))
-
+    make_single_plot(data_country, data_country_forecast, filename,
+      figuresDir, country, logy = FALSE, 
+      ymax = round(max(data_country$deaths), digits = -1))
 }
 
 forecastR0 <- vector(mode = "list", length = nCountries)
@@ -88,22 +86,37 @@ for(ii in 1:nCountries) {
   N <- length(dates[[ii]])
   country <- countries[[ii]]
   
-  forecastR0[[ii]] <- CountryForecastR0(ii,country,dates[[ii]],forecast,predictionR0)
+  forecastR0[[ii]] <- CountryForecastR0(ii,country,dates[[ii]],
+    forecast,predictionR0)
+  
+   write.csv(forecastR0[[ii]], paste0(figuresDir, 'R0_Results-',JOBID,
+      '-',countries[[ii]],'.csv'))
   
   data_countryTest <- CountryOutputs(ii,country,dates[[ii]],reported_cases,
-    deaths_by_country,prediction,estimated.deaths,out$Rt)
+    deaths_by_country, prediction, estimated.deaths, out$Rt)
   
-  data_country_forecastTest <- CountryForecast(ii,country,dates[[ii]],forecast,prediction,
-    estimated.deaths)
-  
-  make_comparison_plot(data_countryTest, data_country_forecastTest, 
-    forecastR0[[ii]], filename, figuresDir, country, logy = TRUE, 
-    include_cases = TRUE, ymax = max(forecastR0[[ii]]$predicted_max))
+  data_country_forecastTest <- CountryForecast(ii,country,dates[[ii]],
+    forecast, prediction, estimated.deaths)
   
   make_comparison_plot(data_countryTest, data_country_forecastTest, 
-    forecastR0[[ii]], paste0(filename, "linear"), figuresDir, country, 
-    logy = FALSE, include_cases = FALSE, 
+    forecastR0[[ii]], "infections", filename, figuresDir, country, 
+    logy = TRUE, include_cases = TRUE, 
     ymax = max(forecastR0[[ii]]$predicted_max))
+  
+  make_comparison_plot(data_countryTest, data_country_forecastTest, 
+    forecastR0[[ii]], "infections", paste0(filename, "linear"), 
+    figuresDir, country, logy = FALSE, include_cases = FALSE, 
+    ymax = max(forecastR0[[ii]]$predicted_max))
+  
+  make_comparison_plot(data_countryTest, data_country_forecastTest, 
+    forecastR0[[ii]], "deaths", filename, figuresDir, country, 
+    logy = TRUE, include_cases = TRUE, 
+    ymax = max(forecastR0[[ii]]$death_max))
+  
+  make_comparison_plot(data_countryTest, data_country_forecastTest, 
+    forecastR0[[ii]], "deaths", paste0(filename, "linear"), 
+    figuresDir, country, logy = FALSE, include_cases = FALSE, 
+    ymax = max(forecastR0[[ii]]$death_max))
 }
 
 
